e46c410af23c1aefe6fd13e865885a6fb6d8b5cf
MakeLord95 <juhani.markus@gmail.com>
2023-11-02 11:06:51 +0200

    ---

--- Week_2/Backend/middleware-lab/server.js
+++ Week_2/Backend/middleware-lab/server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(requestLogger);
+app.use(unknownEndpoint);
+
 app.get("/textmessage", (req, res) => {
   res.send("Text message!");
 });
@@ -35,21 +50,6 @@ app.get("/info", (req, res) => {
 
   res.send(htmlInfo);
 });
-
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(requestLogger);
-app.use(unknownEndpoint);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
