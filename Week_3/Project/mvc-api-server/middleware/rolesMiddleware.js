37739f9016386b2685aec32c78517cdd298f7363
MakeLord95 <juhani.markus@gmail.com>
2023-11-09 21:10:56 +0200

    Week 3 - Project - mvc-api-server Done

--- Week_3/Project/mvc-api-server/middleware/rolesMiddleware.js
+++ Week_3/Project/mvc-api-server/middleware/rolesMiddleware.js
@@ -1,7 +1,11 @@
+const users = require("../models/users");
+
 // Define role-based middleware
 function checkRole(role) {
   return (req, res, next) => {
-    const userRole = req.user && req.user.role;
+    const username = req.query.username;
+
+    const userRole = users.find((user) => user.username === username).role;
 
     if (userRole === role) {
       next(); // User has the required role, continue to the next middleware
